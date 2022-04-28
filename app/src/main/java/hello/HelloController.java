a8748132b7aeb003724b562531a05677bc30b7e7
dev-cyphors <88205567+dev-cyphors@users.noreply.github.com>
2022-04-28 11:54:54 +0530

    Update HelloController.java

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Spring Boot! 0.1.0";
+        return "Hello from Spring Boot! 0.1.1";
     }   
 }
